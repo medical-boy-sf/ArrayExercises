955105b392d48e233a9c1c646d48537c79ce14b5
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-02-09 21:10:49 +0200

    01.addAndSubtract.js

--- 01.addAndSubtract.js
+++ 01.addAndSubtract.js
@@ -1,3 +1,25 @@
 function addAndSubtract(input) {
-    
+    let firstArrSum = 0;
+    for (let i = 0; i < input.length; i++) {
+        firstArrSum += Number(input[i]);
+    }
+
+    let secondArr = [];
+    let secondArrSum = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        if (Number(input[i]) % 2 === 0) {
+            secondArr[i] = Number(input[i]) + i;
+        } else {
+            secondArr[i] = Number(input[i]) - i;
+        }
+    }
+
+    for (let i = 0; i < secondArr.length; i++) {
+        secondArrSum += Number(secondArr[i]);
+    }
+
+    console.log(secondArr);
+    console.log(firstArrSum);
+    console.log(secondArrSum);
 }
