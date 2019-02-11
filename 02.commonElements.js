b1cfa1de33a7b8cea2c4fd281586f56a35cbd21c
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-02-12 00:09:36 +0200

    02.commonElements.js

---
+++ 02.commonElements.js
@@ -0,0 +1,9 @@
+function commonElements(array1, array2) {
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] === array2[j]) {
+                console.log(array1[i]);
+            }
+        }
+    }
+}
